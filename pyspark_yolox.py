369c82165dd740dafab1c2bbee88318404d7cc61
Kacper <kacper.lesnianski@wp.pl>
2022-11-24 14:33:08 +0100

    partitioning, better csv, formatting

--- pyspark_yolox.py
+++ pyspark_yolox.py
@@ -1,7 +1,6 @@
 # Runs yolox-tiny model on images and saves a dictionary
 # of every object detected with confidence value exceeding TRESHOLD for every file.
 
-from yolox.data.datasets import COCO_CLASSES
 from yolox.exp import get_exp
 from yolox.data.data_augment import ValTransform
 from yolox.utils import postprocess
@@ -10,10 +9,13 @@ import torch
 import cv2
 import numpy as np
 import os
+import sys
 import json
 from pyspark.sql import SparkSession
 
 TRESHOLD = 0.4
+HDFS_HOSTNAME= 'brick'
+MIN_PARTITIONS = 8
 
 spark = (
     SparkSession.builder.appName("TEST")
@@ -39,12 +41,8 @@ model.load_state_dict(ckpt["model"])
 brmodel = sc.broadcast(model)
 brexp = sc.broadcast(exp)
 
-# files = sc.binaryFiles("hdfs://brick:9000/images/*/*/*/*.jpg")
-# files = sc.binaryFiles("hdfs://brick:9000/images/0/1/*/*.jpg")
-files = sc.binaryFiles("hdfs://brick:9000/images/0/0/0/*0.jpg")
-
 img_labels_df = spark.read.csv(
-    "hdfs://brick:9000/metadata/train_labels.csv", header=True
+    f"hdfs://{HDFS_HOSTNAME}:9000/metadata/train_labels.csv", header=True
 ).toPandas()
 
 img_labels_df = img_labels_df.set_index(["id"])
@@ -83,7 +81,7 @@ def inference(file):
 def get_predictions(file):
     file_name, outputs = file
     output = outputs[0]
-    file_id = os.path.basename(file_name).split('.')[0]
+    file_id = os.path.basename(file_name).split(".")[0]
 
     if output is None:
         return (file_id, json.dumps({}))
@@ -104,43 +102,66 @@ def get_predictions(file):
     return (file_id, dstr)
 
 
-inf = files.map(lambda f: inference(f))
-predictions = inf.map(lambda f: get_predictions(f))
+if "--cached-pred" in sys.argv:
+    print('Using cached values from results_predictions')
+    predictions_df = spark.read.csv(
+        f"hdfs://{HDFS_HOSTNAME}:9000/results_predictions", header=True, sep=";"
+    )
+else:
+    print('Running object detection on files...')
+    # files = sc.binaryFiles(f"hdfs://{HDFS_HOSTNAME}:9000/images/*/*/*/*.jpg")
+    # files = sc.binaryFiles(f"hdfs://{HDFS_HOSTNAME}:9000/images/0/1/*/*.jpg")
+    files = sc.binaryFiles(f"hdfs://{HDFS_HOSTNAME}:9000/images/0/0/0/*.jpg")
 
-predictions_df = predictions.toDF( ("id", "predictions") )
-predictions_df.write.csv("hdfs://brick:9000/results_predictions", mode="overwrite", header=True)
+    print("Repartitioning small dataset...")
+    if(files.getNumPartitions() < MIN_PARTITIONS):
+        files = files.repartition(MIN_PARTITIONS)
+    print('Repartitioning done')
+
+    inf = files.map(lambda f: inference(f))
+    predictions = inf.map(lambda f: get_predictions(f))
+
+    predictions_df = predictions.toDF(("id", "predictions"))
+    predictions_df.write.csv(
+        f"hdfs://{HDFS_HOSTNAME}:9000/results_predictions", mode="overwrite", header=True, sep=";"
+    )
 
 predictions_df = predictions_df.toPandas()
 predictions_df = predictions_df.set_index(["id"])
 
-labels_to_check = img_labels_bc.value.loc[predictions_df.index , :]
-labels_to_check = np.unique ( np.array([val[0] for val in labels_to_check.values]))
+labels_to_check = img_labels_bc.value.loc[predictions_df.index, :]
+labels_to_check = np.unique(np.array([val[0] for val in labels_to_check.values]))
 
 def count_objects(class_label):
     df = img_labels_bc.value
-    images_of_class = df.loc[df['landmark_id'] == str(class_label)]
-    images_of_class = [ index for index in images_of_class.index] 
+    images_of_class = df.loc[df["landmark_id"] == str(class_label)]
+    images_of_class = [index for index in images_of_class.index]
 
     d = {}
 
     file_counter = 0
     for image_id in images_of_class:
         if image_id in predictions_df.index:
-            image_dict = predictions_df.at[image_id, 'predictions']
+            image_dict = predictions_df.at[image_id, "predictions"]
             image_dict = json.loads(image_dict)
             for key in image_dict:
                 d[key] = d.get(key, 0) + image_dict[key]
-            file_counter+=1
+            file_counter += 1
 
     avgs = {}
     for key in d:
         avgs[key] = d[key] / file_counter
-    
-    #must cast label to str because numpyt str is not accespted by csv writer
-    return (str(class_label), json.dumps(d), json.dumps(avgs))
-            
+
+    # must cast label to str because numpyt str is not accespted by csv writer
+    return (str(class_label), str(file_counter), json.dumps(d), json.dumps(avgs))
+
 
 classes = sc.parallelize(labels_to_check)
 sums = classes.map(count_objects)
-sums_df = sums.toDF(("landmark_id", "predictions", "averages"))
-sums_df.write.csv("hdfs://brick:9000/results_predictions_per_class", mode="overwrite", header=True)
+sums_df = sums.toDF(("landmark_id", "image_count", "predictions_sum", "averages"))
+sums_df.write.csv(
+    f"hdfs://{HDFS_HOSTNAME}:9000/results_predictions_per_class",
+    mode="overwrite",
+    header=True,
+    sep=";",
+)
