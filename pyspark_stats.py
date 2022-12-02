a460ce0f1253415f662c6773a26d28397b2f950e
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-12-02 14:49:34 +0100

    format

--- pyspark_stats.py
+++ pyspark_stats.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import json
 import os
 
-#Hostname of our HDFS namenode
+# Hostname of our HDFS namenode
 HDFS_HOSTNAME = "brick"
 # HDFS_HOSTNAME = "g-furnace"
 
-#Create spark context
+# Create spark context
 spark = (
     SparkSession.builder.appName("stat-generation")
     .config("spark.driver.memory", "4g")
@@ -16,12 +16,12 @@ spark = (
 )
 sc = spark.sparkContext
 
-#Read detections per landmark_id from HDFS
+# Read detections per landmark_id from HDFS
 objects_per_class = spark.read.csv(
     f"hdfs://{HDFS_HOSTNAME}:9000/results_predictions_per_class", header=True, sep=";"
 ).rdd
 
-#Read file with mappings of landmark_id to landmark name
+# Read file with mappings of landmark_id to landmark name
 landmark_names = (
     spark.read.csv(
         f"hdfs://{HDFS_HOSTNAME}:9000/metadata/train_label_to_name.csv",
@@ -32,12 +32,12 @@ landmark_names = (
     .set_index("landmark_id")
 )
 
-#Stat 1: Sum detections of selected object classes for landmarks that start with given letter
+# Stat 1: Sum detections of selected object classes for landmarks that start with given letter
 alphabet = [*ascii_uppercase]
 classes_of_interest = [0, 1, 2, 11, 13]
 detections_per_letter = {}
 
-#return number of detected objects of class _class, but count them only if landmark's name starts with letter
+# return number of detected objects of class _class, but count them only if landmark's name starts with letter
 def count_class_for_letter(entry, letter, _class):
     id = entry["landmark_id"]
     name = landmark_names.at[id, "name"]
@@ -55,7 +55,8 @@ def count_class_for_letter(entry, letter, _class):
 
     return obj_cnt
 
-#Stat 1: Sum detections of selected object classes for landmarks that start with given letter
+
+# Stat 1: Sum detections of selected object classes for landmarks that start with given letter
 for _class in classes_of_interest:
     detections_per_letter[_class] = {}
 
@@ -66,11 +67,11 @@ for _class in classes_of_interest:
         _sum = occurences.sum()
         detections_per_letter[_class][letter] = _sum
 
-#Write results to local FS and HDFS
+# Write results to local FS and HDFS
 Path("./stats/alphabet_count").mkdir(parents=True, exist_ok=True)
 for _class in detections_per_letter:
 
-    #write to local fs
+    # write to local fs
     output = "letter;count\n"
     for letter in detections_per_letter[_class]:
         output += f"{letter};{detections_per_letter[_class][letter]}\n"
@@ -78,9 +79,8 @@ for _class in detections_per_letter:
     with open(os.path.join("./stats/alphabet_count", f"{_class}.csv"), "w+") as f:
         f.write(output)
 
-    #why is there no easy way to write a string to a HDFS file in spark?
+    # why is there no easy way to write a string to a HDFS file in spark?
     df = sc.parallelize([output]).coalesce(1).toDF(("string"))
-    df.write.mode('overwrite').text(f"hdfs://{HDFS_HOSTNAME}:9000/alphabet_count/{_class}")
-
-    
-    
+    df.write.mode("overwrite").text(
+        f"hdfs://{HDFS_HOSTNAME}:9000/alphabet_count/{_class}"
+    )
